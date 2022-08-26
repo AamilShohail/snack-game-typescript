db766d959612742b70d45d3cd662ce1ceafb1a6e
AamilShohail <shohail.17@itfac.mrt.ac.lk>
2022-08-26 07:16:22 +0530

    2 files modified

--- index.ts
+++ index.ts
@@ -4,3 +4,10 @@ import './style.css';
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+const canvas = document.getElementsByTagName<"canvas">(
+  "canvas"
+) as HTMLCollectionOf<HTMLCanvasElement>;
+
+console.log(canvas);
+console.log(canvas[0]);
